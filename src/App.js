675d20880ea84617b28c3b47cc5b64a95bd7b27c
sgneha <sgneha05@gmail.com>
2020-06-30 15:57:48 +0100

    rendering what user types with default as 'react', but rendering a lot and button not working as expected

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import React, { Component, useState, useEffect } from "react";
 const App = () => {
   //state
   const [news, setNews] = useState([]);
+  const [searchQuery, setsearchQuery] = useState("react");
   //fetch news
   const fetchNews = () => {
-    fetch("http://hn.algolia.com/api/v1/search?query=react")
+    fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
       .then((result) => result.json()) //convert it to json
       .then((data) => setNews(data.hits))
       .catch((error) => console.log(error)); //if error
@@ -14,9 +15,16 @@ const App = () => {
   useEffect(() => {
     fetchNews();
   });
+  const handleChange = (e) => {
+    setsearchQuery(e.target.value);
+  };
   return (
     <div>
       <h2>news</h2>
+      <form>
+        <input type="text" value={searchQuery} onChange={handleChange} />
+        <button>Search</button>
+      </form>
       {news.map((n, i) => (
         <p key={i}>{n.title}</p>
       ))}
