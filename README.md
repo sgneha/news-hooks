2cf2b35021b2c7d5810f8073baa81e404f781e1a
sgneha <sgneha05@gmail.com>
2020-06-30 17:55:29 +0100

    enhancing code by adding feature of showing loading

--- README.md
+++ README.md
@@ -19,8 +19,9 @@ Fetch will return a promise.
 
 • Everything is on real time we are making lot of request(on every key stroke). Now we want only fetch the data when search button is clicked.
 • Implement onSubmit method which execute handleSubmit() when form is submitted.Prevent the default behaviour so that page is not free load. Set entire default url in one more state.when user hits submit the setUrl method changes the state of url according to the searchQuery(user input).And run the effect based on url.
+• Now we will show loading indicator when `loading=true`
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
 
--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ const App = () => {
   const [url, setUrl] = useState(
     "http://hn.algolia.com/api/v1/search?query=react"
   );
+  const [loading, setLoading] = useState(false);
   //fetch news
   const fetchNews = () => {
+    //set loading true
+    setLoading(true);
     fetch(url) // when component mounts it takes default url and after that whenever url changes.
       .then((result) => result.json()) //convert it to json
-      .then((data) => setNews(data.hits))
+      .then((data) => (setNews(data.hits), setLoading(false)))
       .catch((error) => console.log(error)); //if error
   };
 
@@ -29,6 +32,7 @@ const App = () => {
   return (
     <div>
       <h2>news</h2>
+      {loading ? <h2>Loading...</h2> : ""}
       <form onSubmit={handleSubmit}>
         <input type="text" value={searchQuery} onChange={handleChange} />
         <button>Search</button>
